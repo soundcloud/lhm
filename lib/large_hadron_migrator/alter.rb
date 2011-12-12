d1c0f970418afe93f84ccbf1095788be1552a95e
Rany Keddo <rany@soundcloud.com>
2011-12-12 11:41:05 +0100

    alter tests and implementation

--- lib/large_hadron_migrator/alter.rb
+++ lib/large_hadron_migrator/alter.rb
@@ -5,18 +5,47 @@
 
 module LargeHadronMigrator
   class Alter
-    attr_accessor :name
+    attr_accessor :name, :changes
 
     def initialize(table)
       @table = table
       @name = @table.name
+      @changes = []
     end
 
-    def changes
-      %Q{
-
-      }
+    def ddl(statement)
+      changes << statement
     end
+
+    def add_column(name, definition = "")
+      ddl = "alter table `%s` add column `%s` %s" % [@name, name, definition]
+      changes << ddl.strip
+    end
+
+    def remove_column(name)
+      ddl = "alter table `%s` drop `%s`" % [@name, name]
+      changes << ddl.strip
+    end
+
+    def add_index(cols, option = "")
+      ddl = "create index `%s` on %s %s" % [idx_name(cols), idx_spec(cols), option]
+      changes << ddl.strip
+    end
+
+    def remove_index(cols)
+      ddl = "drop index `%s` on `%s`" % [idx_name(cols), @name]
+      changes << ddl.strip
+    end
+
+    def idx_name(cols)
+      [*cols].join("_and_") + "_index"
+    end
+
+    private
+
+      def idx_spec(cols)
+        "#{ @name }(#{ cols.join(', ') })"
+      end
   end
 end
 
--- spec/unit/alter_spec.rb
+++ spec/unit/alter_spec.rb
@@ -5,43 +5,103 @@
 
 require File.expand_path(File.dirname(__FILE__)) + '/unit_helper'
 
+def xit(*args); end
+
 describe LargeHadronMigrator::Alter do
   include UnitHelper
 
-  describe "altering" do
+  describe "index changes" do
+    before(:each) do
+      @table = LargeHadronMigrator::Table.new("altering")
+      @alter = LargeHadronMigrator::Alter.new(@table)
+    end
+
+    it "should name index with a single column" do
+      @alter
+        .idx_name(["name"])
+        .must_equal("name_index")
+    end
+
+    it "should name index with a multiple columns" do
+      @alter
+        .idx_name(["name", "firstname"])
+        .must_equal("name_and_firstname_index")
+    end
+
+    it "should add an index" do
+      @alter.add_index(["a", "b"])
+
+      @alter.changes.must_equal [
+        "create index `a_and_b_index` on altering(a, b)"
+      ]
+    end
+
+    it "should remove an index" do
+      @alter.remove_index ["b", "a"]
+
+      @alter.changes.must_equal [
+        "drop index `b_and_a_index` on `altering`"
+      ]
+    end
+  end
+
+  describe "column changes" do
     before(:each) do
       @table = LargeHadronMigrator::Table.new("altering")
       @alter = LargeHadronMigrator::Alter.new(@table)
     end
 
     it "should add a column" do
-      @alter.add_column :logins, "INT(12)"
+      @alter.add_column "logins", "INT(12)"
 
-      flunk
+      @alter.changes.must_equal [
+        "alter table `altering` add column `logins` INT(12)"
+      ]
     end
 
     it "should remove a column" do
-      @alter.remove_column :logins
+      @alter.remove_column "logins"
 
-      flunk
+      @alter.changes.must_equal [
+        "alter table `altering` drop `logins`"
+      ]
     end
+  end
 
-    it "should add an index" do
-      @alter.add_index :logins, :created_at
-
-      flunk
-    end
-
-    it "should remove an index" do
-      @alter.remove_index :logins, :created_at
-
-      flunk
+  describe "direct changes" do
+    before(:each) do
+      @table = LargeHadronMigrator::Table.new("altering")
+      @alter = LargeHadronMigrator::Alter.new(@table)
     end
 
     it "should accept a ddl statement" do
-      @alter.execute "alter table %s add column flag tinyint(1)" % @alter.name
+     ddl = @alter.ddl "alter table `%s` add column `f` tinyint(1)" % @alter.name
 
-      flunk
+     @alter.changes.must_equal [
+      "alter table `altering` add column `f` tinyint(1)"
+    ]
+    end
+
+  end
+
+  describe "multiple changes" do
+    before(:each) do
+      @table = LargeHadronMigrator::Table.new("altering")
+      @alter = LargeHadronMigrator::Alter.new(@table)
+    end
+
+    it "should add two columns" do
+      @alter.add_column "first", "VARCHAR(64)"
+      @alter.add_column "last", "VARCHAR(64)"
+      @alter.changes.length.must_equal 2
+
+      @alter
+        .changes[0]
+        .must_equal "alter table `altering` add column `first` VARCHAR(64)"
+
+      @alter
+        .changes[1]
+        .must_equal "alter table `altering` add column `last` VARCHAR(64)"
     end
   end
 end
