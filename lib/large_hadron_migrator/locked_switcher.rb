bd44c2eec14703d2120b73fa6345891277500e9d
Rany Keddo <rany@soundcloud.com>
2012-01-03 17:40:24 +0100

    autocommit

--- lib/large_hadron_migrator/locked_switcher.rb
+++ lib/large_hadron_migrator/locked_switcher.rb
@@ -2,7 +2,10 @@
 #  copyright (c) 2011, soundcloud ltd., rany keddo, tobias bielohlawek, tobias
 #  schmidt
 #
-#  Switches the names of two tables with a write lock.
+#  Switches the names of two tables with a write lock. Use this as a safe
+#  alternative to rename, which can cause data inconsistencies:
+#
+#    http://bugs.mysql.com/bug.php?id=39675
 #
 
 module LargeHadronMigrator
@@ -15,13 +18,14 @@ module LargeHadronMigrator
 
     def switch
       [
-        "set session autocommit=0",
+        "set @lhm_auto_commit = @@session.autocommit"
+        "set session autocommit = 0",
         "lock table `#{ @origin }` write, `#{ @destination }` write",
         "alter table `#{ @origin }` rename `#{ @archive }`",
         "alter table `#{ @destination }` rename `#{ @origin }`",
         "commit",
         "unlock tables",
-        "set session autocommit=1"
+        "set session autocommit = @lhm_auto_commit"
       ]
     end
   end
