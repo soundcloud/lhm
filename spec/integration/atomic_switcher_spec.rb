0256177ff75c13f237125cf86eb454ac607ff422
Arthur Neves <arthurnn@gmail.com>
2015-04-08 13:17:57 -0400

    Fix indentation

--- spec/integration/atomic_switcher_spec.rb
+++ spec/integration/atomic_switcher_spec.rb
@@ -27,55 +27,55 @@ describe Lhm::AtomicSwitcher do
       Thread.abort_on_exception = false
     end
 
-   it "should retry on lock wait timeouts" do
-     skip "This spec only works with mysql2" unless defined? Mysql2
+    it "should retry on lock wait timeouts" do
+      skip "This spec only works with mysql2" unless defined? Mysql2
 
-     without_verbose do
-       queue = Queue.new
+      without_verbose do
+        queue = Queue.new
 
-       locking_thread = start_locking_thread(10, queue, "DELETE from #{@destination.name}")
+        locking_thread = start_locking_thread(10, queue, "DELETE from #{@destination.name}")
 
-       switching_thread = Thread.new do
-         conn = ar_conn 3306
-         switcher = Lhm::AtomicSwitcher.new(@migration, conn)
-         switcher.retry_sleep_time = 0.2
-         queue.pop
-         switcher.run
-         Thread.current[:retries] = switcher.retries
-       end
+        switching_thread = Thread.new do
+          conn = ar_conn 3306
+          switcher = Lhm::AtomicSwitcher.new(@migration, conn)
+          switcher.retry_sleep_time = 0.2
+          queue.pop
+          switcher.run
+          Thread.current[:retries] = switcher.retries
+        end
 
-       switching_thread.join
-       locking_thread.join
-       assert switching_thread[:retries] > 0, "The switcher did not retry"
-     end
-   end
+        switching_thread.join
+        locking_thread.join
+        assert switching_thread[:retries] > 0, "The switcher did not retry"
+      end
+    end
 
-   it "should give up on lock wait timeouts after MAX_RETRIES" do
-     skip "This spec only works with mysql2" unless defined? Mysql2
+    it "should give up on lock wait timeouts after MAX_RETRIES" do
+      skip "This spec only works with mysql2" unless defined? Mysql2
 
-     without_verbose do
-       queue = Queue.new
-       locking_thread = start_locking_thread(10, queue, "DELETE from #{@destination.name}")
+      without_verbose do
+        queue = Queue.new
+        locking_thread = start_locking_thread(10, queue, "DELETE from #{@destination.name}")
 
-       switching_thread = Thread.new do
-         conn = ar_conn 3306
+        switching_thread = Thread.new do
+          conn = ar_conn 3306
 
-         switcher = Lhm::AtomicSwitcher.new(@migration, conn)
-         switcher.max_retries = 2
-         switcher.retry_sleep_time = 0
-         queue.pop
-         begin
-           switcher.run
-         rescue ActiveRecord::StatementInvalid => error
-           Thread.current[:exception] = error
-         end
-       end
+          switcher = Lhm::AtomicSwitcher.new(@migration, conn)
+          switcher.max_retries = 2
+          switcher.retry_sleep_time = 0
+          queue.pop
+          begin
+            switcher.run
+          rescue ActiveRecord::StatementInvalid => error
+            Thread.current[:exception] = error
+          end
+        end
 
-       switching_thread.join
-       locking_thread.join
-       assert switching_thread[:exception].is_a?(ActiveRecord::StatementInvalid)
-     end
-   end
+        switching_thread.join
+        locking_thread.join
+        assert switching_thread[:exception].is_a?(ActiveRecord::StatementInvalid)
+      end
+    end
 
     it "should raise on non lock wait timeout exceptions" do
       switcher = Lhm::AtomicSwitcher.new(@migration, connection)
